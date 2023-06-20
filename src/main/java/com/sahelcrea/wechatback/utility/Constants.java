50ed76efa4c2a66b7e1d3ce74deb4fbf9b27b6d5
Jeremi-Koloma <kolomajeremi60@gmail.com>
2023-06-19 23:01:04 -0700

    Ajout des constants chemin d'accès des photos

---
+++ src/main/java/com/sahelcrea/wechatback/utility/Constants.java
@@ -0,0 +1,16 @@
+package com.sahelcrea.wechatback.utility;
+
+import java.io.File;
+
+public class Constants {
+    // chemin d'accès pour stocker les photos de profil des utilsateurs
+    public static final String USER_FOLDER = "src/main/resources/static/image/user_profiles//";
+
+
+    //  chemin d'accès pour la photo de profil par défaut de l'utilisateur
+    public static final File TEMP_USER = new File("src/main/resources/static/image/user_profile_temp/profil.png");
+
+
+    // chemin d'accès pour stocker les photos des posts des utilisateurs
+    public static final String POST_FOLDER = "src/main/resources/static/image/posts//";
+}
